3129ba73220e9be3acf6b561c3e2c6ac2043151d
NayemulSifat <nayemulsifatoff@gmail.com>
2020-10-05 20:57:03 +0600

    port and nodemon fixed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./config/beavolunteer-b3e03-firebase-adminsdk-a9fh9-b8081c0973.json");
 require('dotenv').config()
 
-
+const port = 5000
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,7 +16,7 @@ app.use(cors());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kqm2a.mongodb.net/BeAVolunteer?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kqm2a.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
@@ -83,6 +83,6 @@ client.connect(err => {
 });
 
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || port, () => {
   console.log('success')
 })
